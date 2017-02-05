1965d1f02a537fe563aaca83203b2afcf3207853
czulua18 <camilozv18@gmail.com>
2017-02-05 15:42:22 -0500

    base de datos y crear usuario estan listos

--- telematica/api/controllers/UserController.js
+++ telematica/api/controllers/UserController.js
@@ -7,5 +7,26 @@
 
 module.exports = {
 	
+	new:function(req,res){
+		console.log("entre por aqui");
+		res.view();
+	},
+	create: function(req,res){
+		console.log("ola" + req.param('Nombre'));
+		var userObj={
+			Nombre: req.param('Nombre'),
+			Telefono: req.param('Telefono'),
+			Barrio: req.param('Barrio'),
+			Direccion: req.param('Direccion'),
+			email: req.param('email'),
+		}
+		User.create(userObj,function(err,user){
+			if(err){
+				console.log(err);
+				return res.redirect('/user/new');
+			}
+			res.redirect('/user');
+		})
+	}
 };
 
